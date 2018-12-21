ddd57e4d5e500aacc6151db6b133bb7bd76cb367
DLE.NET.TR <dlenettr@users.noreply.github.com>
2018-12-22 01:48:35 +0300

    Create allocine.series.class.php

---
+++ allocine.series.class.php
@@ -0,0 +1,91 @@
+<?php
+/*
+=====================================================
+ Author : Mehmet Hanoğlu <dle.net.tr>
+-----------------------------------------------------
+ License : MIT License
+-----------------------------------------------------
+ Copyright (c)
+-----------------------------------------------------
+ Date : 22.12.2018 [1.5]
+=====================================================
+*/
+
+error_reporting(E_ALL ^ E_WARNING);
+ini_set('display_errors', 1);
+
+class FilmReader {
+
+	public function get( $url ) {
+		$html = $this->getURLContent( $url );
+
+		$dom = new DOMDocument();
+		@$dom->loadHTML( $html );
+		$x = new DOMXPath( $dom );
+
+		$film['url'] = $this->cleanWords( $url );
+		$film['name'] = $this->cleanWords( $x->query('//meta[@property="og:title"]')->item(0)->getAttribute("content") );
+		$film['img'] = $this->cleanWords( $x->query('//meta[@property="og:image"]')->item(0)->getAttribute("content") );
+		$film['orgimg'] = $this->cleanWords( $x->query('//meta[@property="og:image"]')->item(0)->getAttribute("content") );
+		$_tmp = [];
+		foreach($x->query('//div[@class="meta-body-item serie-genre"]/span/div') as $node) { $_tmp[] = $this->cleanWords( $node->nodeValue ); }
+		$film['genres'] = implode( ", ", $_tmp );
+		foreach($x->query('//div[@class="stats-numbers-row-item"]') as $node) {
+			$v = $this->cleanWords( $node->nodeValue );
+			if ( strpos( $v, "Saisons" ) !== false ) $film['seasons'] = str_replace( "Saisons", "", $v );
+			else if ( strpos( $v, "Episodes" ) !== false ) $film['episodes'] = str_replace( "Episodes", "", $v );
+		}
+		$film['story'] = $this->cleanWords( $x->query('//div[contains(@class,"content-txt")]')->item(0)->nodeValue );
+
+		$_tmp1 = [];
+		foreach($x->query('//a[contains(@href,"/personne/")]') as $node) {
+			$_tmp1[] = $this->cleanWords( $node->nodeValue );
+		}
+		$_tmp2 = [];
+		foreach($x->query('//div[contains(@class,"card card-person")]/div/div[@class="meta-title"]/*') as $node) {
+			$_tmp2[] = $this->cleanWords( $node->nodeValue );
+		}
+
+		$film['actors'] = implode(", ", $_tmp2);
+		$film['director'] = implode(", ", array_diff($_tmp1,$_tmp2));
+
+		preg_match("#[0-9]{4}#s", $this->cleanWords( $x->query('//div[@class="meta-body-item meta-body-info"]')->item(0)->nodeValue ), $m);
+		$film['year'] = $m[0];
+
+		//$film['year'] = $this->cleanWords( trim( $x->query('//div[@class="serie-production"]/span')->item(0)->nodeValue, '()' ) );
+		$film['country'] = $this->cleanWords( $x->query('//span[contains(@class,"nationality")]')->item(0)->nodeValue );
+		if ( $x->query('//div[@class="serie-production"]/span')->item(1) ) {
+			$film['runtime'] = $this->cleanWords( $x->query('//div[@class="serie-production"]/span')->item(1)->nodeValue );
+		}
+		if ( $x->query('//span[@class="stareval-note"]')->item(1) ) {
+			$film['ratinga'] = $this->cleanWords( $x->query('//span[@class="stareval-note"]')->item(1)->nodeValue );
+		}
+		$film['ratingb'] = "10";
+		if ( $x->query('//span[@class="stareval-review"]')->item(1) ) {
+			$film['ratingc'] = strstr( $this->cleanWords( $x->query('//span[@class="stareval-review"]')->item(1)->nodeValue ), ' ', true);
+		}
+		$film['status'] = $this->cleanWords( $x->query('//div[contains(@class,"label-status")]')->item(0)->nodeValue );
+
+		return $film;
+	}
+
+	private function cleanWords( $text ) {
+		$text = trim( str_replace( array( "\t", "  ", "\"", "\r\n", "\n", "»", "&#187;", "&raquo;" ), "", $text ) );
+		return $text;
+	}
+
+	private function getURLContent($url) {
+		$ch = curl_init( $url );
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_ENCODING, "utf-8");
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 120);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 120);
+		curl_setopt($ch, CURLOPT_MAXREDIRS, 2);
+		$output  = curl_exec( $ch );
+		curl_close( $ch );
+		return $output;
+	}
+}
+
+@header( "Content-type: text/html; charset=utf-8" );
